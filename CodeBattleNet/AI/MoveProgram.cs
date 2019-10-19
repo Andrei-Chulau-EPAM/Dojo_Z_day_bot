6122c7d21662b1808018d4e80697708006dc21b1
Andrey <andrei_chulau@epam.com>
2019-10-19 17:06:53 +0300

    fix restriction

--- CodeBattleNet/AI/MoveProgram.cs
+++ CodeBattleNet/AI/MoveProgram.cs
@@ -72,7 +72,7 @@ namespace CodeBattleNet.AI
         public Movement GetMovementToClosestEnemy()
         {
             var player = _client.GetPlayerTank();
-            var closestEnemyPosition = PlayerUtility.GetClosestEnemyPosition(_client, out var movementRoad, 50);
+            var closestEnemyPosition = PlayerUtility.GetClosestEnemyPosition(_client, out var movementRoad, 30);
 
             if (closestEnemyPosition.IsNegativePoint())
             {
